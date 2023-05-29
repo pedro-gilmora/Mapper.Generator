35283936a77b1cd57b7eb7b0318592e80e2b7ada
pedro.gilmora <pedro.gilmora@outlook.es>
2023-05-29 08:47:41 -0400

    MSBuild fix for package referencing

--- SourceCrafter.MappingGenerator.UnitTests/AssemblyInfo.cs
+++ SourceCrafter.MappingGenerator.UnitTests/AssemblyInfo.cs
@@ -5,7 +5,7 @@
         ignore: SourceCrafter.Mapping.Constants.Ignore.OnSource
     )]
 #endif
-//[assembly: 
-//    SourceCrafter.Mapping.Attributes.Map<
-//        SourceCrafter.UnitTests.IUser,
-//        SourceCrafter.UnitTests.UserDto>]
+[assembly:
+    SourceCrafter.Mapping.Attributes.Map<
+        SourceCrafter.UnitTests.User,
+        SourceCrafter.UnitTests.UserDto>]
